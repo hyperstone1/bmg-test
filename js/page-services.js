29985eb7bf0ba301ef4e3055364b2ef2e95e51c3
hyperstone1 <kingofthegorgon@mail.ru>
2023-06-08 18:24:54 +0300

    add slider for services (need to change layout)

---
+++ js/page-services.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 3,
+  centeredSlides: true,
+  loop: true,
+  navigation: {
+    nextEl: '.slider-next',
+    prevEl: '.slider-prev',
+  },
+});
