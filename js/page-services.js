093da2d26703ca992aa94b27495c4c4e19b26523
hyperstone1 <kingofthegorgon@mail.ru>
2023-06-13 13:11:42 +0300

    add brokeridge to link

--- js/page-services.js
+++ js/page-services.js
@@ -1,7 +1,17 @@
 var swiper = new Swiper('.swiper-container', {
   slidesPerView: 3,
   centeredSlides: true,
+  // spaceBetween: 80,
   loop: true,
+  // effect: 'coverflow',
+  // grabCursor: true,
+  // coverflowEffect: {
+  //   rotate: 30,
+  //   stretch: 0,
+  //   depth: 100,
+  //   modifier: 1,
+  //   // slideShadows: true,
+  // },
   navigation: {
     nextEl: '.slider-next',
     prevEl: '.slider-prev',
